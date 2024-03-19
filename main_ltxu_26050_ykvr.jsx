c6a8110e477ca9eb32db9b4f99a33f7a58bb7998
Hordieiev <hordieivivan@gmail.com>
2024-03-19 06:12:01 +0200

    Fixed log output

---
+++ main_ltxu_26050_ykvr.jsx
@@ -0,0 +1,59 @@
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const filterEvenNumbers = numbers => numbers.filter(isEven);
+
+const filterEvenNumbers = numbers => numbers.filter(isEven);
+71,19,62,13,48,83,47,16,91,70,44,66,54,16,9,57,50,34,77,74,77,26,88,79,39,28,73,46,33,89,79,23,14,83,72,23,27,23,71,55,81,25,57,93,15,79,10,47,16,82,59,99 - 72
+let result = performOperation(getRandomNumber(), getRandomNumber());
+banana
+
+function addNumbers(a, b) { return a + b; }
+const reverseString = str => str.split("").reverse().join("");
+const countOccurrences = (array, value) => array.reduce((count, current) => (current === value ? count + 1 : count), 0);
+const getUniqueValues = array => [...new Set(array)];
+orange
+
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+true + 29,79,63,22,96,87,86,91,28,84,21,89,4,28,63,54,89,78,44,50,33,41,54,75,88,24,23,42,10,51,76,30,28,84,50,61,98,24,45,29,64,26,70,16,56,60,71,56,55,91,57
+const filterEvenNumbers = numbers => numbers.filter(isEven);
+apple
+
+const getRandomElement = array => array[getRandomIndex(array)];
+orange - 97
+const timeoutPromise = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
+18,83,96,3,25,12,23,21,23,93,56,47,28,68,21,62,17,55,6,28,7,83,66,46,52,85,77,44,25,97,67,2,61,12,38,29,93,78,36 + 43
+const squareRoot = num => Math.sqrt(num);
+
+// This is a comment
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+orange + true
+const sumOfPrimes = limit => { const isPrime = num => { for (let i = 2; i < num; i++) if (num % i === 0) return false; return num > 1; }; return Array.from({ length: limit }, (_, index) => index).filter(isPrime).reduce((sum, prime) => sum + prime, 0); };
+
+51 + 73
+const getRandomIndex = array => Math.floor(Math.random() * array.length);
+const getRandomIndex = array => Math.floor(Math.random() * array.length);
+
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+apple * false
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+const filterObject = (obj, predicate) => Object.fromEntries(Object.entries(obj).filter(([key, value]) => predicate(value)));
+
+21 * true
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+
+80,92,80,83,66,60,1,58 * 5
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
+orange
+
+const squareRoot = num => Math.sqrt(num);
+let array = getRandomArray(); array.forEach(item => console.log(item));
+const deepClone = obj => JSON.parse(JSON.stringify(obj));
+// This is a comment
+false - true
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+apple
+
+const rotateArrayLeft = (array, k) => { const rotations = k % array.length; return [...array.slice(rotations), ...array.slice(0, rotations)]; };
+55 + 9,5,89
+const getUniqueCharacters = str => Array.from(new Set(str)).join("");
+const getUniqueValues = array => [...new Set(array)];
+
